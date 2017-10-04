f6badb4f5cfef29ce50004a026a93108d71ecca8
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2017-10-04 09:15:23 +0000

    All the required Data Fields , properties are included in the class.

--- NAmeDayApp/NAmeDayApp/NameDayModel.cs
+++ NAmeDayApp/NAmeDayApp/NameDayModel.cs
@@ -8,5 +8,12 @@ namespace NAmeDayApp
 {
     class NameDayModel
     {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public IEnumerable<string> Name { get; set; }
+
+        public string NameAsString => string.Join(", ", Name);
+            
     }
+
 }
