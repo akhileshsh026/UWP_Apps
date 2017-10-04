4df0827114c2ca6c3c5a3cfe6daa70fd2db49e0f
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2017-10-04 09:54:58 +0000

    Giving the data by using the model class. from the Code behind main class

--- NAmeDayApp/NAmeDayApp/MainPageData.cs
+++ NAmeDayApp/NAmeDayApp/MainPageData.cs
@@ -8,7 +8,21 @@ namespace NAmeDayApp
 {
     class MainPageData
     {
+        public string _greeting { get; set; } = "Hello World";
 
-     
+        public List<NameDayModel> NameDays { get; set; }
+
+        public MainPageData()
+        {
+            NameDays = new List<NameDayModel>();
+
+            for (int month = 1; month <= 12; month++)
+            {
+                NameDays.Add(new NameDayModel( 19, month, new string[] { "Akhilesh KS" }));
+                NameDays.Add(new NameDayModel(14, month, new string[] { "Bittu","Allwin","Cow" }));
+                NameDays.Add(new NameDayModel(16, month, new string[] { "Akash", "Saurabh", "Kallu" }));
+            }
+        }
+
     }
 }
